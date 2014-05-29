09455646a64ebed81efed37b283dabe280a96f0a
tq5124 <wutianqi124@gmail.com>
2014-05-30 00:17:21 +0800

    增加对系统环境变量的转换

--- py/lib/regex.py
+++ py/lib/regex.py
@@ -1,8 +1,15 @@
-import re
+import re, os
 
 def rePath(path):
 	# clean the args of a path
 	# clean the "\"" of a path
+	try:
+		# replace the system var like %PATH%
+		replace_pre = re.match('%[a-zA-Z]*%', path).group(0)
+		replace_aft = os.environ.get(replace_pre)
+		path.replace(replace_pre, replace_aft)
+	except:
+		pass
 	try:
 		# clean path like ""C:\Program Files\VMware\VMware Tools\vmtoolsd.exe" -n vmusr"
 		path = re.match('\".*\"', path).group(0)
