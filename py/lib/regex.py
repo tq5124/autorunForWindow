beb52668126893b1ab42416f01b96d21fdc8030c
tq5124 <wutianqi124@gmail.com>
2014-05-30 13:55:04 +0800

    regex改用findall方法！match只适合句首的情况

--- py/lib/regex.py
+++ py/lib/regex.py
@@ -5,14 +5,15 @@ def rePath(path):
 	# clean the "\"" of a path
 	try:
 		# replace the system var like %PATH%
-		replace_pre = re.match('%[a-zA-Z]*%', path).group(0)
-		replace_aft = os.environ.get(replace_pre)
-		path.replace(replace_pre, replace_aft)
+		replace_array = re.findall('%[a-zA-Z]*%', path)
+		for replace_pre in replace_array:
+			replace_aft = os.environ.get(replace_pre[1:-1])
+			path = path.replace(replace_pre, replace_aft)
 	except:
 		pass
 	try:
 		# clean path like ""C:\Program Files\VMware\VMware Tools\vmtoolsd.exe" -n vmusr"
-		path = re.match('\".*\"', path).group(0)
+		path = re.findall('\".*\"', path)[0]
 		path = path[1:-1]
 	except:
 		pass
