8b4a7e9f877a20e3efa28a0ca924491ab5e6207a
tq5124 <wutianqi124@gmail.com>
2014-05-31 17:25:14 +0800

    补充output文件夹

---
+++ py/output/logon.js
@@ -0,0 +1,89 @@
+var logon = [
+    {
+        "keys": [
+            {
+                "path": "C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe", 
+                "Publisher": "VMware, Inc.", 
+                "name": "VMware User Process", 
+                "Description": "VMware Tools Core Service"
+            }
+        ], 
+        "path": "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", 
+        "*": "keys"
+    }, 
+    {
+        "keys": [
+            {
+                "path": "C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe", 
+                "Publisher": "VMware, Inc.", 
+                "name": "VMware User Process", 
+                "Description": "VMware Tools Core Service"
+            }
+        ], 
+        "path": "HKLM\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run", 
+        "*": "keys"
+    }, 
+    {
+        "keys": [
+            {
+                "path": "C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe", 
+                "Publisher": "VMware, Inc.", 
+                "name": "VMware User Process", 
+                "Description": "VMware Tools Core Service"
+            }
+        ], 
+        "path": "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", 
+        "*": "keys"
+    }, 
+    {
+        "keys": [
+            {
+                "path": "C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe", 
+                "Publisher": "VMware, Inc.", 
+                "name": "VMware User Process", 
+                "Description": "VMware Tools Core Service"
+            }
+        ], 
+        "path": "HKCU\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run", 
+        "*": "keys"
+    }, 
+    {
+        "path": "c:\\windows\\system32\\rdpclip.exe", 
+        "Publisher": "Microsoft Corporation", 
+        "*": "key", 
+        "name": "rdpclip", 
+        "Description": "RDP Clip \u76d1\u89c6\u7a0b\u5e8f"
+    }, 
+    {
+        "path": "C:\\Windows\\system32\\userinit.exe", 
+        "Publisher": "Microsoft Corporation", 
+        "*": "key", 
+        "name": "C:\\Windows\\system32\\userinit.exe,", 
+        "Description": "Userinit \u767b\u5f55\u5e94\u7528\u7a0b\u5e8f"
+    }, 
+    {
+        "path": "C:\\Windows\\SysWOW64\\explorer.exe", 
+        "Publisher": "Microsoft Corporation", 
+        "*": "key", 
+        "name": "explorer.exe", 
+        "Description": "Windows \u8d44\u6e90\u7ba1\u7406\u5668"
+    }, 
+    {
+        "keys": [
+            {
+                "path": "C:\\Users\\tq5124\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup", 
+                "Publisher": "", 
+                "name": "desktop.ini", 
+                "Description": ""
+            }, 
+            {
+                "path": "C:\\Users\\tq5124\\AppData\\Roaming\\Wandoujia2\\Applications\\2.76.0.6280\\wandoujia_helper.exe", 
+                "Publisher": "", 
+                "name": "wandoujia_helper.lnk", 
+                "Description": ""
+            }
+        ], 
+        "path": "C:\\Users\\tq5124\\AppData\\Roaming\\Wandoujia2\\Applications\\2.76.0.6280\\wandoujia_helper.exe", 
+        "*": "files"
+    }
+]
