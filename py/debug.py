0c99f25263ed922fb68256d56c532ecc7eaf3f6b
tq5124 <wutianqi124@gmail.com>
2014-06-03 23:12:33 +0800

    删除多余的文件

--- py/debug.py
+++
@@ -1,9 +0,0 @@
-import wmi
-c = wmi.WMI ()
-
-stopped_services = c.Win32_Service (StartMode="Auto")
-if stopped_services:
-  for s in stopped_services:
-    print s.Caption, "service is not running"
-else:
-  print "No auto services stopped"
